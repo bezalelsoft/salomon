669c1465181d77538f624ed4542080d26e049670
bezalelsoft <bezalelsoft@gmail.com>
2022-07-11 21:27:39 +0200

    logging fixed

--- src/salomon/impl/CopyModelPackage.py
+++ src/salomon/impl/CopyModelPackage.py
@@ -1,14 +1,13 @@
-import boto3, os, time
-from pprint import pprint
+import boto3, os, time, tempfile, logging
 import docker
-import tempfile
 from .s3_helper import parse_s3_url
 
 
 def copy_model_package(
         source_arn: str, dst_group_name: str, dst_s3_path: str, dst_ecr: str,
         src_session: boto3.session.Session = boto3.session.Session(),
-        dst_session: boto3.session.Session = boto3.session.Session()
+        dst_session: boto3.session.Session = boto3.session.Session(),
+        docker_client: docker.client.DockerClient = docker.client.from_env()
 ):
     """
     Makes a copy of SageMaker Model Package.
@@ -20,14 +19,16 @@ def copy_model_package(
     5. Pulls docker images and then pushes to `dst_ecr`
     6. Creates new SageMaker Model Package in current AWS account.
 
-    :param source_arn:
-    :param dst_group_name:
-    :param dst_s3_path:
-    :param dst_ecr:
-    :param src_session: boto3.session.Session, used only in source AWS account
-    :param dst_session: boto3.session.Session, used only in destination AWS account
-    :return:
+    :param source_arn: source model package ARN.
+    :param dst_group_name: target model package group name.
+    :param dst_s3_path: target S3 path, where model files will be copied to.
+    :param dst_ecr: target ECR, where images will be copied to.
+    :param src_session: boto3.session.Session, used only in source AWS account.
+    :param dst_session: boto3.session.Session, used only in destination AWS account.
+    :param docker_client: docker.client.DockerClient
+    :return: ARN of created model
     """
+    logger = logging.getLogger(__name__)
     src_sm = src_session.client('sagemaker')
     dst_sm = dst_session.client('sagemaker')
     src_model_package = src_sm.describe_model_package(ModelPackageName=source_arn)
@@ -35,16 +36,17 @@ def copy_model_package(
     dst_model_package, files_to_copy, docker_images_to_copy = rebuild_model_package(
         src_model_package, dst_group_name, dst_s3_path, dst_ecr
     )
-    pprint(dst_model_package)
-    pprint(files_to_copy)
-    pprint(docker_images_to_copy)
+
+    logger.debug(dst_model_package)
+    logger.debug(files_to_copy)
+    logger.debug(docker_images_to_copy)
 
     copy_files(files_to_copy, src_session, dst_session)
-    copy_docker_images(docker_images_to_copy)
+    copy_docker_images(docker_images_to_copy, docker_client)
 
     response = dst_sm.create_model_package(**dst_model_package)
     print("Model copy completed.")
-    pprint(response)
+    logger.debug(response)
     return response.get("ModelPackageArn")
 
 
@@ -59,7 +61,6 @@ def rebuild_model_package(src_model_package: dict, dst_group_name: str, dst_s3_p
         if k not in dont_copy_keys:
             dst_model_package[k] = v
 
-    # dst_model_package['Tags'] = []
     # dst_model_package['ModelPackageName'] = dst_name      # gives error: botocore.exceptions.ClientError: An error occurred (ValidationException) when calling the CreateModelPackage operation: 1 validation errors detected:Environment variable map cannot be specified when using non-versioned ModelPackages
     dst_model_package['ModelPackageGroupName'] = dst_group_name
 
@@ -101,8 +102,6 @@ def join_uri(path: str, filename: str) -> str:
 
 
 def prepare_docker_urls(src_uri: str, dst_ecr: str):
-    # 492215442770.dkr.ecr.eu-central-1.amazonaws.com/sagemaker-scikit-learn:0.23-1-cpu-py3
-
     src_repository, src_tag = src_uri.split(":")
     if "/" in src_repository:
         src_image = src_repository.split("/")[1]
@@ -119,6 +118,7 @@ def copy_files(
         src_session: boto3.session.Session,
         dst_session: boto3.session.Session
     ):
+    logger = logging.getLogger(__name__)
     with tempfile.TemporaryDirectory() as temp_dir:
         src_s3 = src_session.client('s3')
         dst_s3 = dst_session.client('s3')
@@ -128,43 +128,36 @@ def copy_files(
             filename = os.path.basename(src)
             temp_file = os.path.join(temp_dir, filename)
 
-            print(f"Downloading {src} to temp file {temp_file}")
+            logger.debug(f"Downloading {src} to temp file {temp_file}")
             src_s3.download_file(src_tuple[0], src_tuple[1], temp_file)
 
-            print(f"Uploading {dst}")
+            logger.debug(f"Uploading {dst}")
             dst_s3.upload_file(temp_file, dst_tuple[0], dst_tuple[1])
 
-            print(f"Deleting {temp_file}")
+            logger.debug(f"Deleting {temp_file}")
             os.remove(temp_file)
 
-            # copy_source = {
-            #     'Bucket': src_tuple[0],
-            #     'Key': src_tuple[1]
-            # }
-            # print(f"Copying from {src} to {dst}")
-            # s3.meta.client.copy(copy_source, dst_tuple[0], dst_tuple[1])
 
-
-def copy_docker_images(images_to_copy: list):
-    docker_client = docker.from_env()
+def copy_docker_images(images_to_copy: list, docker_client: docker.client.DockerClient):
+    logger = logging.getLogger(__name__)
     for src, dst in images_to_copy:
         src_repository, src_tag = src.split(":")
         dst_repository, dst_tag = dst.split(":")
 
-        print(f"Pulling image {src_repository}:{src_tag}")
+        logger.debug(f"Pulling image {src_repository}:{src_tag}")
         image = docker_client.images.pull(repository=src_repository, tag=src_tag)
 
-        print(f"Tagging image {src_repository}:{src_tag} with {dst_repository}:{dst_tag}")
+        logger.debug(f"Tagging image {src_repository}:{src_tag} with {dst_repository}:{dst_tag}")
         image.tag(repository=dst_repository, tag=dst_tag)
 
-        print(f"Pushing image {dst_repository}:{dst_tag}")
+        logger.debug(f"Pushing image {dst_repository}:{dst_tag}")
 
         prev_ts = time.time() - 10
         for line in docker_client.api.push(repository=dst_repository, tag=dst_tag, stream=True, decode=True):
             ts = time.time()
             if ts - prev_ts > 10:
                 prev_ts = ts
-                print(line)
+                logger.debug(line)
 
 
 def list_docker_images_in_model_package(model_package_arn: str, session: boto3.session.Session = boto3.session.Session()):
